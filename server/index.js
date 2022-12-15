d969f7fb3095807c574be02b11404819861abc8d
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-15 15:56:17 -0700

    Creando el servidor con node

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const express = require("express")
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT | 3001
+
+const app = express()
+
+// .listen() methos help us to start a server. It will constinously listen to a server and respond
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Connected at port ${PORT}`)
+}) 
