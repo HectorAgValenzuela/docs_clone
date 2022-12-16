90d52465d33f459d53f8a6d8d144e6aefb64a01d
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-15 18:49:06 -0700

    Conexion a mongodb

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const express = require("express")
 const mongoose = require('mongoose')
 
@@ -5,7 +7,17 @@ const PORT = process.env.PORT | 3001
 
 const app = express()
 
-// .listen() methos help us to start a server. It will constinously listen to a server and respond
+// Lets connect the the app to mongodb
+
+mongoose.connect(process.env.DB_URI).then(() => {
+    console.log("Connection successful")
+}).catch((err) => {
+    console.log(err)
+})
+
+// .listen() methods help us to start a server. It will constinously listen to a server and respond
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Connected at port ${PORT}`)
 }) 
+
+// Lets connect the the app to mongodb
