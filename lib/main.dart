0be6fe8544c15699735ef80217f20240c5a95057
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-22 16:45:05 -0700

    Añadiendo el paquete de .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:docs_clone_flutter/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+
+  await dotenv.load(fileName: ".env");
+
   runApp(const ProviderScope(
     child: MyApp(),
   ));
