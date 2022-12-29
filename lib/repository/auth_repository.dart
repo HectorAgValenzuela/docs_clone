03639526848322f76f49b0a009d1f814462789ea
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-29 14:57:28 -0700

    Usando el almacenamiento local para autenticar un usuario

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -2,13 +2,17 @@ import 'dart:convert';
 
 import 'package:docs_clone_flutter/models/error_model.dart';
 import 'package:docs_clone_flutter/models/user_model.dart';
+import 'package:docs_clone_flutter/repository/local_storage_repository.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart';
 
-final authRepositoryProvider = Provider(
-    (ref) => AuthRepository(googleSignIn: GoogleSignIn(), client: Client()));
+final authRepositoryProvider = Provider((ref) => AuthRepository(
+      googleSignIn: GoogleSignIn(),
+      client: Client(),
+      localStorageRepository: LocalStorageRepository(),
+    ));
 
 final userProvider = StateProvider<UserModel?>(
   (ref) => null,
@@ -17,12 +21,15 @@ final userProvider = StateProvider<UserModel?>(
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
   final Client _client;
+  final LocalStorageRepository _localStorageRepository;
 
-  AuthRepository({
-    required GoogleSignIn googleSignIn,
-    required Client client,
-  })  : _googleSignIn = googleSignIn,
-        _client = client;
+  AuthRepository(
+      {required GoogleSignIn googleSignIn,
+      required Client client,
+      required LocalStorageRepository localStorageRepository})
+      : _googleSignIn = googleSignIn,
+        _client = client,
+        _localStorageRepository = localStorageRepository;
 
   Future<ErrorModel> signInWithGoogle() async {
     ErrorModel error = ErrorModel(
@@ -51,10 +58,10 @@ class AuthRepository {
         switch (res.statusCode) {
           case 200:
             final newUser = userAcc.copyWith(
-              uid: jsonDecode(res.body)['user']['_id'],
-              token: jsonDecode(res.body)['token']
-            );
+                uid: jsonDecode(res.body)['user']['_id'],
+                token: jsonDecode(res.body)['token']);
             error = ErrorModel(error: null, data: newUser);
+            _localStorageRepository.setToken(newUser.token);
             break;
         }
       }
@@ -65,4 +72,36 @@ class AuthRepository {
     return error;
   }
 
+  Future<ErrorModel> getUserData() async {
+    ErrorModel error =
+        ErrorModel(error: "Some unexpected error ocurred", data: null);
+    try {
+      String? token = await _localStorageRepository.getToken();
+      if (token != null) {
+        final host = dotenv.get('IP', fallback: "");
+        var res = await _client.get(Uri.parse('$host/'), headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x_auth-token': token,
+        });
+
+        switch (res.statusCode) {
+          case 200:
+            final newUser = UserModel.fromJson(
+              jsonEncode(
+                jsonDecode(res.body)['user'],
+              ),
+            ).copyWith(
+              token: token,
+            );
+            error = ErrorModel(error: null, data: newUser);
+            _localStorageRepository.setToken(newUser.token);
+            break;
+        }
+      }
+
+    } catch (e) {
+      error = ErrorModel(error: e.toString(), data: null);
+    }
+    return error;
+  }
 }
