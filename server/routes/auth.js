e136aee202199dd63d0d9ce75b567f95385e2e66
HectorAgValenzuela <hector-av1@hotmail.com>
2022-12-29 13:58:57 -0700

    Implementando peticiòn get con el middleware auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,12 @@
 const express = require("express");
 const User = require("../models/user");
 
+// jwt is for securely transmiting information 
+// it will store the user information in the local storage of the device, so you don't need to sign in again
+// it's important to know that we don't store de ID for security reasons, hacking stuff
+const jwt = require('jsonwebtoken');
+const auth = require("../middleware/auth");
+
 // This is the classic way
 // const app = express()
 
@@ -10,7 +16,6 @@ const User = require("../models/user");
 const authRouter = express.Router();
 
 authRouter.post('/api/signup', async (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
     try {
         const {name, email, profilePic} = req.body // the body parameter allow us to access the data of req
         // also its the .headers parameter, its usually for the auth token
@@ -29,11 +34,21 @@ authRouter.post('/api/signup', async (req, res) => {
             user = await user.save()
         }
 
-        res.json({user})
+        const token = jwt.sign({id: user._id}, "passwordKey")
 
-
+        res.json({user, token})
     } catch (e) {
         res.status(500).json({error: e.message})
+    }
+})
+
+// 
+authRouter.get('/', auth, async (req, res) => {
+    const user = await User.findById(req.user)
+    try {
+        res.json({user, token: req.token})
+    } catch (e) {
+        res.status(500).json({ error: e.message })
     }
 })
 
