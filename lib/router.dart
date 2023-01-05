365ae557198727eaf72e7bdffa1405d24e0432fb
HectorAgValenzuela <hector-av1@hotmail.com>
2023-01-04 23:13:52 -0700

    Creando router para ir a home

--- lib/router.dart
+++ lib/router.dart
@@ -1,3 +1,4 @@
+import 'package:docs_clone_flutter/screens/home_screen.dart';
 import 'package:docs_clone_flutter/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:routemaster/routemaster.dart';
@@ -5,5 +6,11 @@ import 'package:routemaster/routemaster.dart';
 final loggedOutMap = RouteMap(routes: {
   '/': (route) => const MaterialPage(
         child: LoginScreen(),
+      )
+});
+
+final loggedInMap = RouteMap(routes: {
+  '/': (route) => const MaterialPage(
+        child: HomeScreen(),
       )
 });
