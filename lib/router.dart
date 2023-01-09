4e123e13a22af6e24e69c92186ab7c3ff87eb74f
HectorAgValenzuela <hector-av1@hotmail.com>
2023-01-08 23:26:11 -0700

    Creando la ruta para el documento con su id

--- lib/router.dart
+++ lib/router.dart
@@ -1,3 +1,4 @@
+import 'package:docs_clone_flutter/screens/document_screen.dart';
 import 'package:docs_clone_flutter/screens/home_screen.dart';
 import 'package:docs_clone_flutter/screens/login_screen.dart';
 import 'package:flutter/material.dart';
@@ -12,5 +13,8 @@ final loggedOutMap = RouteMap(routes: {
 final loggedInMap = RouteMap(routes: {
   '/': (route) => const MaterialPage(
         child: HomeScreen(),
-      )
+      ),
+  '/document/:id': (route) => MaterialPage(
+        child: DocumentScreen(id: route.pathParameters['id'] ?? ''),
+      ),
 });
