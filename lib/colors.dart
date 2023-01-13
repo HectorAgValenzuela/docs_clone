407887d590b74e1c73d9d78408c8d30df883b2da
HectorAgValenzuela <hector-av1@hotmail.com>
2023-01-12 19:37:50 -0700

    Creando un navbar estático

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kWhiteColor = Colors.white;
 const kBlackColor = Colors.black;
 const kRedColor = Colors.red;
+const kBLueColor = Color.fromRGBO(26, 115, 232, 1);
--- lib/screens/document_screen.dart
+++ lib/screens/document_screen.dart
@@ -1,3 +1,4 @@
+import 'package:docs_clone_flutter/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,9 +15,59 @@ class DocumentScreen extends ConsumerStatefulWidget {
 }
 
 class _DocumentScreenState extends ConsumerState<DocumentScreen> {
+  TextEditingController titleContoller =
+      TextEditingController(text: 'Untitled');
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleContoller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kWhiteColor,
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.lock,
+                size: 16,
+              ),
+              label: const Text('Share'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kBLueColor,
+              ),
+            ),
+          ),
+        ],
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/images/docs-logo.png',
+              height: 40,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            SizedBox(
+              width: 150,
+              child: TextField(
+                controller: titleContoller,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(left: 10),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       body: Center(
         child: Text(widget.id),
       ),
